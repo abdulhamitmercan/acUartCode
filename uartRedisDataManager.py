c50c6b3b92c8aa1452874885ab0382594d236655
abdulhamitmercan <abdulhamitmercan25@gmail.com>
2024-10-04 11:42:27 +0300

    hele baah

--- uartRedisDataManager.py
+++ uartRedisDataManager.py
@@ -50,7 +50,7 @@ class ReadDataResponseManager:
             await self.redis_client.hset("Uart", "errorType", readDataResponse.getErrorType())
             await self.redis_client.hset("Uart", "chargingStatus", readDataResponse.getChargingStatus())
             await self.redis_client.hset("Uart", "connectorStatus", readDataResponse.getConnectorStatus())
-            # await self.redis_client.hset("Uart", "deviceId", readDataResponse.getDeviceId())
+            await self.redis_client.hset("Uart", "deviceId", readDataResponse.getDeviceId())
             await self.redis_client.hset("Uart", "chargeFinished", readDataResponse.getEVChargeTermination())
 
             # print("redis read data response set")
