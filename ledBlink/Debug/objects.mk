9d2b896740de9109932726cf6fb072fa6c01088d
feyzanoz <ozverfeyzanur@gmail.com>
2024-10-23 06:01:22 +0000

    data_received fonksiyonu kullanıldı

--- ledBlink/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibmath.a -l"C:/ti/msp430ware_3_80_14_01/iqmathlib/libraries/CCS/MPY32/5xx_6xx/IQmathLib.a" -llibc.a
-
